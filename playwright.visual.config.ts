19d7753ccf5914341b40a7688531f6ea1017c589
Marco Ciampini <marco.ciampo@gmail.com>
2026-01-27 23:39:16 +0100

    Address more feedback

--- playwright.visual.config.ts
+++ playwright.visual.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './tests/visual',
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1, // Sequential for consistent screenshots
